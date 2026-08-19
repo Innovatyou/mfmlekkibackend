80c03174146eac0c1c6041333a7aef59d5b4269f
Innovatyou <ittrinityfinancial@gmail.com>
2026-08-19 08:10:54 +0100

    Deploy splash-free Flutter web login

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3917076289" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3839924664" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
